da027f66943d77484adca710d412afa317b6565c
wkdang <36007806+wkdang@users.noreply.github.com>
2018-02-04 02:40:07 +0000

    Add files via upload

--- scripts/small.sh
+++ scripts/small.sh
@@ -145,7 +145,7 @@ sedcmd3="s/root_user=root/root_user=$HANAUSR/g"
 sedcmd4="s/root_password=AweS0me@PW/root_password=$HANAPWD/g"
 sedcmd5="s/sid=H10/sid=$HANASID/g"
 sedcmd6="s/number=00/number=$HANANUMBER/g"
-cat hdbinst.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
+cat hdbinst1.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
 echo "hana prepare end" >> /tmp/parameter.txt
 
 #!/bin/bash
